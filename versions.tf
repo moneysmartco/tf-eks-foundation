7ebff5a493b83e90f6c4f1849b2deedcc168bc83
Alex <alex@MacBook-Air.local>
2022-03-03 16:55:39 +0530

    Added provoder for template

--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.3.0"
     }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
   }
   required_version = ">= 0.13"
 }
